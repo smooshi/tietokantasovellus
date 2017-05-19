0cb76f44fe54f69e833d36bb19e944eb83e4abe3
smooshi <smooshi@users.noreply.github.com>
2017-05-19 10:17:45 +0300

    Web issues

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' +os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+#removed from Procfile, stored here for reasons.
+#web: gunicorn app:app --preload
