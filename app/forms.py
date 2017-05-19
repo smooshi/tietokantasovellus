b117ccbdebeced6aa86b7935dde5f4001ce9eaee
smooshi <smooshi@users.noreply.github.com>
2017-05-19 10:12:26 +0300

    askldjalk heroku

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,13 @@
-from flask_wtf import FlaskForm
+from flask_wtf import Form
 from wtforms import StringField, BooleanField, PasswordField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
+class LoginForm(Form):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
 
-class CreateForm(FlaskForm):
+class CreateForm(Form):
     username = StringField('username', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
