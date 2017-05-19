3d939dbec8614f3e6d9b2c8b92a2b5cdbc72a7fa
smooshi <smooshi@users.noreply.github.com>
2017-05-19 09:58:40 +0300

    Herokufix

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,13 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, PasswordField
 from wtforms.validators import DataRequired
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
 
-class CreateForm(Form):
+class CreateForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
